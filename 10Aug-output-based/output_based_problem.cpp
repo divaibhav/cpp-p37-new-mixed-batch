c6e792d9a3b274d2c1fdc9ffe2ac7888d4a624c4
Vaibhav Diwan <divaibhav@gmail.com>
2024-08-10 14:25:37 +0530

    first commit

---
+++ 10Aug-output-based/output_based_problem.cpp
@@ -0,0 +1,8 @@
+/*
+WAP tp print your name, roll number 
+and address on the output
+screen / console
+
+in the below mentioned format
+Vaibhav Diwan, 81, Kolar Road
+*/
---
+++ 10Aug-output-based/output_demo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Name - Vaibhav Diwan" << "\n";
+    cout << "Email - " << "divaibhav@gmail.com" << "\n";   
+    cout << "Mobile - " << "9874563210" << "\n";
+
+   
+
+    return 0;
+
+}
+// Name - Vaibhav Diwan
+// email - divaibhav@gmail.com
+// Mobile - 3216547891
