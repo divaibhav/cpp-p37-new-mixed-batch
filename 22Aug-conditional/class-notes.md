c546a70d7a95c0efd85fc802694e4c314b049756
Vaibhav Diwan <divaibhav@gmail.com>
2024-08-22 13:46:59 +0530

    Class notes

---
+++ 22Aug-conditional/class-notes.md
@@ -0,0 +1,4 @@
+![pic1](./resource/pic1.png)
+![pic2](./resource/pic2.png)
+![pic3](./resource/pic3.png3)
+![pic4](./resource/pic4.png4)
---
+++ 22Aug-conditional/if_demo.cpp
@@ -0,0 +1,21 @@
+// WAP to print helloworld if user enter number greater 
+// than 10. But always print your name
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number = 0;
+    cout << "Enter an integer number" << "\n";
+    cin >> number;
+
+    // condition
+    if(number > 10)
+    {
+        cout << "Hello World" << "\n";
+    }
+    cout << "Vaibhav Diwan" << "\n";
+}
+
+/*
+WAP to print your name if user enters 10.
+*/
